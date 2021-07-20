5eec1913fad121614e90ae9a87cac544e201757f
Raju <gangaraju.battem@gmail.com>
2021-07-20 16:22:23 +0530

    camel netty pub sub model - first cut
    camel netty pub sub model - first cut

---
+++ poc-so-camel-netty4-tcp/src/main/java/com/raju/apache/camel/api/netty/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.raju.apache.camel.api.netty;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+}
---
+++ poc-so-camel-netty4-tcp/src/main/java/com/raju/apache/camel/api/netty/services/MessageService.java
@@ -0,0 +1,56 @@
+package com.raju.apache.camel.api.netty.services;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import io.netty.channel.Channel;
+import org.apache.camel.Exchange;
+import org.apache.camel.Handler;
+import org.apache.camel.component.netty4.NettyConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+  
+  private static final Logger log = LoggerFactory.getLogger(MessageService.class);
+
+  private List<String> messageStore = new LinkedList<>();
+
+  /*public void sendToTCP(final String message) {
+    log.info("[Service] - Sending Message over TCP Channel --> {}", message);
+  }*/
+
+  /*@Handler
+  public void receiveFromTCP(final Exchange exchange) {
+    final String messageFromTcp = exchange.getIn().getBody(String.class);
+    log.info("[Service] - Message Received from TCP Channel --> {}", messageFromTcp);
+    this.messageStore.add(messageFromTcp);
+  }*/
+
+  public List<String> getReceivedMessages() {
+    return messageStore;
+  }
+
+
+  private io.netty.channel.Channel openedChannel;
+
+  public void sendToTCP(final String message) {
+    log.info("[Service] - Sending Message over TCP Channel --> {}", message);
+    log.info("[Service] - Channel is Active? {}", this.openedChannel.isActive());
+    log.info("[Service] - Channel is Open? {}", this.openedChannel.isOpen());
+    log.info("[Service] - Channel is Writeble? {}", this.openedChannel.isWritable());
+
+    this.openedChannel.write(message);
+    this.openedChannel.flush();
+  }
+
+  @Handler
+  public void receiveFromTCP(final Exchange exchange) {
+    this.openedChannel = exchange.getProperty(NettyConstants.NETTY_CHANNEL, Channel.class);
+    final String messageFromTcp = exchange.getIn().getBody(String.class);
+    log.info("[Service] - Message Received from TCP Channel --> {}", messageFromTcp);
+    this.messageStore.add(messageFromTcp);
+  }
+}
